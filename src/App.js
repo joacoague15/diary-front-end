e69d35c701a87f39f9d259f999392c9f9319eec9
joacoague15 <joacoague15@gmail.com>
2024-08-05 16:57:16 -0300

    Added styles for message and profile name and images

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import {useEffect, useState} from "react";
 import axios from "axios";
+import {Button, Col, Container, Form, Row} from "react-bootstrap";
 
 function App() {
     const [scrapUrl, setScrapUrl] = useState("");
@@ -33,24 +34,58 @@ function App() {
             console.error("Error fetching data", error);
         }
     }
-  return (
-      <div className="App">
-          <h1>Hello</h1>
-          <input
-              type="text"
-              value={scrapUrl}
-              onChange={(e) => setScrapUrl(e.target.value)}
-              placeholder="Enter link to scrap"
-          />
-          <button onClick={handleAnalyze}>Analyze</button>
-          {ragResponse && <p>{ragResponse}</p>}
-          <div>
-              {chatCompletionResponse && chatCompletionResponse.map((response, index) => (
-                  <p key={index}>{response}</p>
-              ))}
-          </div>
-      </div>
-  );
+
+    const handleImage = (name) => {
+        if (name === "lucia")
+            return <img src="/lucia.webp" alt="lucia" style={{ width: '50px', height: '50px', borderRadius: '50%', marginRight: 20 }} className="mr-3"/>
+        else if (name === "mateo")
+            return <img src="/mateo.webp" alt="mateo" style={{ width: '50px', height: '50px', borderRadius: '50%', marginRight: 20 }} className="profile-image mr-3"/>
+    }
+
+    const capitalizeFirstLetter = (name) => {
+        if (!name) return "";
+        return name.charAt(0).toUpperCase() + name.slice(1);
+    };
+
+    return (
+        <div className="App">
+            <Container className="mt-5">
+              <Row className="justify-content-center">
+                  <Col md={8}>
+                      <Form>
+                          <Form.Group controlId="scrapUrl">
+                              <Form.Control
+                                  size="lg"
+                                  type="text"
+                                  value={scrapUrl}
+                                  onChange={(e) => setScrapUrl(e.target.value)}
+                                  placeholder="Link completo para scrapear"
+                                  style={{fontSize: '2rem', textAlign: "center"}}
+                              />
+                          </Form.Group>
+                          <div className="d-flex justify-content-center">
+                              <Button variant="primary" onClick={handleAnalyze} className="mt-3">
+                                  Analizar
+                              </Button>
+                          </div>
+                      </Form>
+                      {ragResponse && <p className="mt-3" style={{ fontWeight: 'bold', fontSize: '1.5rem' }}>{ragResponse}</p>}
+                      <div className="mt-3">
+                          {chatCompletionResponse && chatCompletionResponse.map((response, index) => (
+                              <div key={index} className="d-flex align-items-start mb-3">
+                                  {handleImage(response.name)}
+                                  <div className="ml-3" >
+                                      <h5 className="mb-1">{capitalizeFirstLetter(response.name)}</h5>
+                                      <p className="mb-0">{response.message}</p>
+                                  </div>
+                              </div>
+                          ))}
+                      </div>
+                  </Col>
+              </Row>
+            </Container>
+        </div>
+    );
 }
 
 export default App;
